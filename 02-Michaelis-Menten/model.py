46f3d071eaee2da6a853f3732f06b8d4b429dd99
ankushgoyalengineer <bioengineer.ankushgoyal@gmail.com>
2026-09-12 23:04:12 +0530

    Updated model.py with corrected code

--- 02-Michaelis-Menten/model.py
+++ 02-Michaelis-Menten/model.py
@@ -5,7 +5,7 @@ def simulate_michael_menten(
     e_initial: float,
     es_initial: 0.0,
     p_initial: 0.0,
-    p_approx: 0.0, 
+    p_approx_initial: 0.0, 
     dt: float = 0.001, 
     k1: float = 10.0,
     k2: float = 2.0,
@@ -21,31 +21,31 @@ def simulate_michael_menten(
     p_coordinates = []
     p_approx_coordinates = []
 
-    for i in range(steps):
+    for i in range(50000):
         time_coordinates.append(current_time)
-        s_coordinates.append(s_conc)
-        e_coordinates.append(e_conc)
-        es_coordinates.append(es_conc)
-        p_coordinates .append(p_conc)
-        p_approx_coordinates.append(p_approx)
-        ds = -(k1 * e_conc * s_conc * dt) + (k2 * es_conc * dt)
-        de = -(k1 * e_conc * s_conc * dt) + (k2 * es_conc * dt) + (k3 * es_conc * dt)
-        des = (k1 * e_conc * s_conc * dt) - (k2 * es_conc * dt) - (k3 * es_conc * dt)
-        dp = (k3 * es_conc * dt)
-        vmax = k3 * (e_conc + es_conc)
-        v = (vmax * s_conc)/(km + s_conc)
+        s_coordinates.append(s_initial)
+        e_coordinates.append(e_initial)
+        es_coordinates.append(es_initial)
+        p_coordinates .append(p_initial)
+        p_approx_coordinates.append(p_approx_initial)
+        ds = -(k1 * e_initial * s_initial * dt) + (k2 * es_initial * dt)
+        de = -(k1 * e_initial * s_initial * dt) + (k2 * es_initial * dt) + (k3 * es_initial * dt)
+        des = (k1 * e_initial * s_initial * dt) - (k2 * es_initial* dt) - (k3 * es_initial * dt)
+        dp = (k3 * es_initial * dt)
+        vmax = k3 * (e_initial + es_initial)
+        v = (vmax * s_initial)/(km + s_initial)
         dp_approx = v * dt
         
-        s_conc += ds
-        e_conc += de
-        es_conc += des
-        p_conc += dp
-        p_approx += dp_approx
+        s_initial += ds
+        e_initial += de
+        es_initial += des
+        p_initial += dp
+        p_approx_initial += dp_approx
         current_time += dt
 
-    return time_coordinates, s_coordinates, e_coordinates, es_coordinates, p_coordinates, p_approx_coordinates, s_initial, e_initial, es_initial, p_initial
+    return time_coordinates, s_coordinates, e_coordinates, es_coordinates, p_coordinates, p_approx_coordinates, s_initial, e_initial, es_initial, p_initial, p_approx_initial
 
-def plot_result(time_coordinates, s_coordinates, e_coordinates, es_coordinates, p_coordinates, p_approx_coordinates, s_initial, e_initial, es_initial, p_initial):
+def plot_result(time_coordinates, s_coordinates, e_coordinates, es_coordinates, p_coordinates, p_approx_coordinates, s_initial, e_initial, es_initial, p_initial, p_approx_initial):
     plt.plot(time_coordinates, s_coordinates, label="Substrate concentration", color="blue")
     plt.plot(time_coordinates, e_coordinates, label="Enzyme concentration", color="red")
     plt.plot(time_coordinates, es_coordinates, label="ES Complex concentration", color="green")
